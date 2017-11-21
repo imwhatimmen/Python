7135fa0b588b5ee77ca9d38929e4e9b5c47025fb
imwhatimmen <33655393+imwhatimmen@users.noreply.github.com>
2017-11-21 11:31:09 +0100

    Delete Vocales a.py

--- Vocales a.py
+++
@@ -1,9 +0,0 @@
-def vocales_a():
-    palabra=raw_input("Dime algo plis: ")
-    suma=0
-    for cont in range(0,len(palabra),1):
-        if palabra[cont]=="a":
-            suma=suma+1
-    print "En la palabra",palabra,"hay",suma,"letras a"
-vocales_a()
-    
