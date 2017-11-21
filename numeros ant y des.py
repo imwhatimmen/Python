67f4e78e05d88bafa639c27cd5430fcf9175ab32
imwhatimmen <33655393+imwhatimmen@users.noreply.github.com>
2017-11-21 11:32:33 +0100

    Delete numeros ant y des.py

--- numeros ant y des.py
+++
@@ -1,7 +0,0 @@
-def numeros():
-    numero=input("Dime un numero ")
-    for cont in range(numero-3,numero,1):
-            print "Los anteriores son ",cont
-    for cont in range(numero+1,numero+4,1):
-            print "Los siguientes son ",cont
-numeros()
