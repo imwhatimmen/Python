011642013cc6d55c8c943b9f1eac73eedf22cff0
imwhatimmen <33655393+imwhatimmen@users.noreply.github.com>
2017-11-21 11:25:14 +0100

    Delete Cadena nombres.py

--- Cadena nombres.py
+++
@@ -1,6 +0,0 @@
-def cadenas():
-    nombre=raw_input("Dime tu nombre: ")
-    apellido1=raw_input("Dime tu apellido: ")
-    apellido2=raw_input("Dime tu apellido: ")
-    print "Tu nombre es", nombre+" ",apellido1+" ",apellido2+" "
-cadenas()
