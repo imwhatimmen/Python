03c93a40357609f0c5a4f284a905dd01bf876395
imwhatimmen <33655393+imwhatimmen@users.noreply.github.com>
2017-11-21 11:26:51 +0100

    Delete Multiplicador.py

--- Multiplicador.py
+++
@@ -1,7 +0,0 @@
-def multiplicador():
-    n=input("Dime que tabla quieres amo: ")
-    print"TABLA DEL",n
-    for cont in range(1,11,1):
-        print n,"x",cont,"=",n*cont
-multiplicador() 
-                     
