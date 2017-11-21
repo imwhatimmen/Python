3648997117e7aa3111722a0925ae77aec4a27358
imwhatimmen <33655393+imwhatimmen@users.noreply.github.com>
2017-11-21 11:30:22 +0100

    Delete Sumador 2.py

--- Sumador 2.py
+++
@@ -1,9 +0,0 @@
-def sumador():
-    x=input ("Dime un numero")
-    y=input ("Dime otro")
-    z=input ("Otro mas")
-    v=input ("Uno mas")
-    w=input ("Dime el ultimo")
-    print "La respuesta es"
-    print x,"+",y,"+",z,"+",v,"+",w,"=",x+y+z+v+w
-sumador()
